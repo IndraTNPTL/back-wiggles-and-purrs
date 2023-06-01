105e18f80f4ec387af9f70bfe9b892ea0ad4c5be
John Doe <crystinekoh@gmail.com>
2023-06-01 11:09:35 +0200

    done

--- db/User.seed.js
+++ db/User.seed.js
@@ -8,18 +8,25 @@ const MONGODB_URI = "mongodb://127.0.0.1:27017/back-wiggles-and-purrs";
 const User = require("../models/User.model");
 
 const User = [
-	{
-		firstname: "",
-		lastname: "",
-		email: "",
-		password: "",
-		role: "",
-	},
-	{
-		firstname: "",
-		lastname: "",
-		email: "",
-		password: "",
-		role: "",
-	},
+  {
+    firstname: "Indra",
+    lastname: "Tinot-Patole",
+    email: "indra@purrs.com",
+    password: "1234",
+    role: "admin",
+  },
+  {
+    firstname: "Crystine",
+    lastname: "Koh",
+    email: "crystine@wiggles.com",
+    password: "4321",
+    role: "admin",
+  },
+  {
+    firstname: "Eli",
+    lastname: "Harb",
+    email: "eli@thesphere.com",
+    password: "6789",
+    role: "user",
+  },
 ];
