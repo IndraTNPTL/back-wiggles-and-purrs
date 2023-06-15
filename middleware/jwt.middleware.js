b303a67ca80decd0dc92a664e282959f696ec31d
Indra Tinot-Patole <indra.tinot.patole@gmail.com>
2023-06-15 18:20:44 +0200

    fixes

--- middleware/jwt.middleware.js
+++ middleware/jwt.middleware.js
@@ -3,64 +3,64 @@ const jsonWebToken = require("jsonwebtoken");
 const User = require("../models/User.model");
 
 // Define the 'isAuthenticated' middleware to check if the user is authenticated
-// const isAuthenticated = async (req, res, next) => {
-// 	// Extract the token from the request headers
-// 	let token = req.headers.authorization;
-// 	console.log(token);
-
-// 	// If no token is found, return an error message
-// 	if (!token) {
-// 		return res.status(400).json({ message: "No token found!" });
-// 	}
-// 	// Remove the 'Bearer ' prefix from the token because Bearer is a convention term to identify the token's bearer. It is not part of the token body
-// 	token = token.replace("Bearer ", "");
-
-// 	// Verify the token using the secret key
-// 	const userToken = jsonWebToken.verify(token, process.env.TOKEN_SECRET);
-
-// 	console.log(userToken);
-
-// 	try {
-// 		// Find the user based on the username in the token
-// 		const user = await User.findById(userToken._id);
-// 		if (!user) {
-// 			return res.status(400).json({ message: "Invalid token" });
-// 		}
-// 		// Attach the user object to the request object
-// 		req.user = user;
-// 		// Once everything went well, go to the next middleware
-// 		next();
-// 	} catch (error) {
-// 		return res.status(401).json({ message: "Invalid token" });
-// 	}
-// };
-
 const isAuthenticated = async (req, res, next) => {
-	let token = req.headers.authorization.split(" ")[1];
+	// Extract the token from the request headers
+	let token = req.headers.authorization;
 	console.log(token);
 
+	// If no token is found, return an error message
 	if (!token) {
 		return res.status(400).json({ message: "No token found!" });
 	}
+	// Remove the 'Bearer ' prefix from the token because Bearer is a convention term to identify the token's bearer. It is not part of the token body
+	token = token.replace("Bearer ", "");
+
+	// Verify the token using the secret key
+	const userToken = jsonWebToken.verify(token, process.env.TOKEN_SECRET);
+
+	console.log(userToken);
 
 	try {
-		const userToken = jsonWebToken.verify(token, process.env.JWTPRIVATEKEY);
-
-		if (!userToken) {
+		// Find the user based on the username in the token
+		const user = await User.findById(userToken._id);
+		if (!user) {
 			return res.status(400).json({ message: "Invalid token" });
 		}
-
-		req.user = userToken;
-
 		// Attach the user object to the request object
-		req.user = await User.findById(userToken._id);
-
+		req.user = user;
 		// Once everything went well, go to the next middleware
 		next();
 	} catch (error) {
 		return res.status(401).json({ message: "Invalid token" });
 	}
 };
+
+// const isAuthenticated = async (req, res, next) => {
+// 	let token = req.headers.authorization.split(" ")[1];
+// 	console.log(token);
+
+// 	if (!token) {
+// 		return res.status(400).json({ message: "No token found!" });
+// 	}
+
+// 	try {
+// 		const userToken = jsonWebToken.verify(token, process.env.JWTPRIVATEKEY);
+
+// 		if (!userToken) {
+// 			return res.status(400).json({ message: "Invalid token" });
+// 		}
+
+// 		req.user = userToken;
+
+// 		// Attach the user object to the request object
+// 		req.user = await User.findById(userToken._id);
+
+// 		// Once everything went well, go to the next middleware
+// 		next();
+// 	} catch (error) {
+// 		return res.status(401).json({ message: "Invalid token" });
+// 	}
+// };
 
 // Define the 'isAdmin' middleware to check if the user has an 'admin' role
 const isAdmin = (req, res, next) => {
